84c12c4d0416b9ecdf9f03d2ad175e561e71dd74
Excep7ion <mehdiguettaf@hotmail.fr>
2024-12-27 00:28:15 +0100

    add ft_printf

---
+++ ft_printf/ft_putnbr_unsigned.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_unsigned.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meguetta <meguetta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 20:08:18 by meguetta          #+#    #+#             */
+/*   Updated: 2024/12/04 15:03:30 by meguetta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putnbr_unsigned(unsigned int nb)
+{
+	int	count;
+
+	count = 0;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr_unsigned(-nb);
+	}
+	else if (nb > 9)
+	{
+		count += ft_putnbr_unsigned(nb / 10);
+		ft_putnbr_unsigned(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+	return (count + 1);
+}
